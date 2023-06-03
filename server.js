9f5fef4c2735446bde41d30c73b30f59920ad8c7
T.J <125562166+TJCole03@users.noreply.github.com>
2023-06-03 09:02:56 -0400

    Add files via upload
    Was having issues committing my work from the terminal, so I found this route. Will be sure to ask for help to see what's gone wrong.

--- server.js
+++ server.js
@@ -1,73 +1,66 @@
-const express = require('express'); //"I require the modules from express"
-const fs = require('fs'); //reading the file systems
-const port = 4000;
+require('dotenv').config()
+const express = require('express')
+const PORT = process.env.PORT || 3002
 const app = express();
+const mongoose = require('mongoose')
+const jsxEngine = require('jsx-view-engine');
+const Item = require('./models/item');
 
 app.use(express.json());
+app.set('view engine', 'jsx')
+app.engine('jsx', jsxEngine())
 
-let frank = 'Never sneak up on a man whos been in a chemical fire';
-
-app.get('/', (req, res) => { //callback function for the route
-    res.send('<h1>Holla</h1>'); 
-});
-
-app.get('/greeting', (req, res) => { //another callback function for the root
-    res.send(`${frank}`);
-});
-
-app.get('/greeting/easter_egg', (req, res) => { //another callback function where I added a second layer
-    res.send('<h1>"I NEED TO GO TO MY QUIET PLACE!!"</h1>');
-});
-
-app.get('/greeting/:name', (req, res) => { 
-    //parseInt(req.params.name)
-    //res.send('Never sneak up on a man whos been in a chemical fire, ' + req.params.name)
-    res.send(`${frank} , ` + req.params.name) //make into one or the other. 
-});
-
-
-                                       //req res
-app.get('/tip/:total/:tippercentage', (req, res) => {
-    let tip = (req.params.total * req.params.tippercentage) / 100
-    res.send(`tip: ${tip}`)    //algebra II 
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.once('open', () => {
+    console.log('connected to mongodb')
 })
 
+let items = [];
 
-app.get('/magic/:input', (req, res) => {
-    parseInt(req.params.magic)
-    let responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
-    let response = Math.floor(Math.random() * responses.length) 
-    const item = responses[response]
-    res.send(`<h1>${item}</h1>`) 
-    return item
+                //callback function; handler
+app.get('/items', (req, res) => {
+    res.json(items);
 })
+//creating new data on server
+app.post('/items', async (req, res) => {
+    if (req.body.name && req.body.price) {
+    } else {
+        res.status(400).send({ message: error.message })
+    } try {
+        const createItem = await Item.create(req.body)
+        res.redirect(`/items/${createItem._id}`)
+    } catch (error) {
+        res.status(400).send({ message: error.message })
+    }
+});
+
+app.get('/items/:id', async (req, res) => {
+    try {
+        const foundItem = await Item.findOne({ _id: req.params.id })
+        res.render('items/Show', {
+            item: foundItem
+        })
+    } catch (error) {
+        res.status(400).send({ message: error.message })
+    }
+})
+
+app.put('items/:id', (req, res) => {
+    const item = items.find(x => x.id === parseInt(req.params.id))
+    if (!item) return res.status(400).json({ message: "Item not found" });
     
-
-//fibonacci 
-// app.get('/fibonacci/:userinput', (req, res) = {
-//     parseInt(req.params.num)
-//     function checkFibonacci(num) {
-//         let newSum = 0; 
-//         let firstNum = 1; 
-//         let secondNum = 2; 
-//         while (num > newSum) {
-//             newSum = firstNum + secondNum
-//             if (newSum === num) { return true }
-//             firstNum = secondNum 
-//             secondNum = newSum
-//         }
-//         return false
-//     }
-//     let num;
-//     if (checkFibonacci === true) {
-//         console.log(`${num} is Lateralus`)
-//     } else {
-//         console.log('Vicarious')
-//     }
-// })
-
-
-app.listen(4000, function () { //telling application to listen on port 4000. (or any port number)
-    console.log(`Listening on ${port}`);
+    Object.assign(item, req.body);
+    res.json(item);
 });
 
+app.delete('/items/:id', (req, res) => {
+    const index = items.findIndex(x => x.id === parseInt(req.params.id));
+    if (index === -1) return res.status(404).json({ message: "Item not found" });
+
+    items.splice(index, 1);
+    res.sendStatus(204);
+})
+
+app.listen(PORT, function () {
+    console.log("listening 3002")
+}) 
